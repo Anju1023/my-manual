dd0824a9066f96cdc02f8340fea98b19c06f02e3
Anju <niafuguapricottaro@gmail.com>
2025-12-19 16:22:58 +0900

    fix: 修正されたタイトルのスペースとレイアウトのリンクを追加

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,9 +1,18 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { BookIcon } from 'lucide-react';
 
 export function baseOptions(): BaseLayoutProps {
 	return {
 		nav: {
-			title: '南部センターベーカリー 業務マニュアル',
+			title: '南部センター ベーカリー 業務マニュアル',
 		},
+		githubUrl: 'https://github.com/Anju1023/my-manual',
+		links: [
+			{
+				icon: <BookIcon />,
+				text: 'マニュアル',
+				url: '/docs',
+			},
+		],
 	};
 }
