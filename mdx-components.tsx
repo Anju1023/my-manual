40c2f0ffc24f9c83ac9ea19403b53c0cc59053f0
Anju <niafuguapricottaro@gmail.com>
2025-12-22 15:33:14 +0900

    feat: カスタムMDXコンポーネントにコードブロックを追加し、画像のズーム機能を強化

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,4 @@
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
@@ -6,6 +7,11 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	return {
 		...defaultMdxComponents,
 		img: (props) => <ImageZoom {...(props as any)} className="rounded-lg" />,
+		pre: ({ ref: _ref, ...props }) => (
+			<CodeBlock keepBackground {...props}>
+				<Pre>{props.children}</Pre>
+			</CodeBlock>
+		),
 		...components,
 	};
 }
